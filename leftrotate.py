e26848a83a7557ec581f0fbc70b74d096754fb52
leelaprasad14 <62127610+leelaprasad14@users.noreply.github.com>
2020-03-14 08:16:33 +0530

    Create leftrotate.py

---
+++ leftrotate.py
@@ -0,0 +1,28 @@
+def leftrotate(arr,n,d):
+    d = d % n 
+    g_c_d = gcd(d, n) 
+    for i in range(g_c_d):               
+        temp = arr[i] 
+        j = i 
+        while 1: 
+            k = j + d 
+            if k >= n: 
+                k = k - n 
+            if k == i: 
+                break
+            arr[j] = arr[k] 
+            j = k 
+        arr[j] = temp 
+def gcd(a,b):
+	if b==0:
+		return a;
+	else :
+		return gcd(b,a%b)
+	
+
+
+n=int(input())
+d=int(input())
+arr=list(map(int,input().split()))
+leftrotate(arr,n,d)
+print(arr)
